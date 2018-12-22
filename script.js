905c1e522246770347504da552cc9a560e954d2b
r000909 <r000909@pc-000909-4.local>
2018-12-23 02:02:55 +0900

    change snow

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-$(function() {
-  $(document).snowfall({
-    minSize  : 1,    // 雪の最小サイズ
-    maxSize  : 8,    // 雪の最大サイズ
-    minSpeed : 1,    // 雪の最低速度
-    maxSpeed : 8,    // 雪の最高速度
-    round    : true, // 雪の形を丸くする
-    shadow   : true, // 雪に影をつける
-    flakeColor : "#fff", // 雪の色を指定
-  });
-});
-
 var array = [
   {
     title: 'White snow',
